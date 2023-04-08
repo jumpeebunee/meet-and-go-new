76cb14cab5abb101d8d1044b4f2fd13aae7cf755
Ben <jumpeebunee@gmail.com>
2023-04-08 18:20:29 +0300

    feat: add events button

--- meetAndGo/src/components/AppMap.tsx
+++ meetAndGo/src/components/AppMap.tsx
@@ -1,11 +1,13 @@
 import { YMaps, Map } from '@pbe/react-yandex-maps'
 import { API_KEY, MAP_CENTER } from '../data/yamapsApi'
 import cl from '../styles/AppMap.module.scss'
+import EventsButton from './EventsButton'
 
 const AppMap = () => {
   return (
     <YMaps query={{apikey: API_KEY}}>
       <Map className={cl.appMap} defaultState={MAP_CENTER}/>
+      <EventsButton/>
     </YMaps>
   )
 }
---
+++ meetAndGo/src/components/EventsButton.tsx
@@ -0,0 +1,11 @@
+import cl from '../styles/eventsButton.module.scss'
+
+const EventsButton = () => {
+  return (
+    <button className={cl.eventsButton}>
+      <span></span>
+    </button>
+  )
+}
+
+export default EventsButton
