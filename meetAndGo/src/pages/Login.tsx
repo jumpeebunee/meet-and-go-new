218ef197480517e992f50a0e51e7773b77ec8ad1
Ben <jumpeebunee@gmail.com>
2023-04-08 09:54:14 +0300

    feat: finish login page

--- meetAndGo/src/pages/Login.tsx
+++ meetAndGo/src/pages/Login.tsx
@@ -1,14 +1,21 @@
-import { IonContent, IonPage } from "@ionic/react"
+import { IonContent, IonPage, IonRouterLink } from "@ionic/react"
 import AuthBanner from "../components/AuthBanner"
 import LoginForm from "../components/LoginForm"
+import cl from '../styles/loginPage.module.scss'
 
 const Login = () => {
   return (
     <IonPage>
       <IonContent>
         <div className="container auth__container">
-          <AuthBanner/>
-          <LoginForm/>
+          <div className={cl.loginPageContent}>
+            <AuthBanner/>
+            <LoginForm/>
+            <p className={cl.loginPageToggle}>
+              Первый раз?
+              <IonRouterLink routerLink="/register"><span> Создать аккаунт</span></IonRouterLink>
+            </p>
+          </div>
         </div>
       </IonContent>
     </IonPage>
