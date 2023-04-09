9805be006726da4b4d129cc88ed3f0ad0f1e335c
Ben <jumpeebunee@gmail.com>
2023-04-09 09:53:00 +0300

    feat: add search button

--- meetAndGo/src/components/AppMap.tsx
+++ meetAndGo/src/components/AppMap.tsx
@@ -2,12 +2,14 @@ import { YMaps, Map } from '@pbe/react-yandex-maps'
 import { API_KEY, MAP_CENTER } from '../data/yamapsApi'
 import cl from '../styles/AppMap.module.scss'
 import EventsButton from './EventsButton'
+import SearchButton from './SearchButton'
 
 const AppMap = () => {
   return (
     <YMaps query={{apikey: API_KEY}}>
       <Map className={cl.appMap} defaultState={MAP_CENTER}/>
       <EventsButton/>
+      <SearchButton/>
     </YMaps>
   )
 }
---
+++ meetAndGo/src/components/SearchButton.tsx
@@ -0,0 +1,11 @@
+import cl from '../styles/SearchButton.module.scss'
+
+const SearchButton = () => {
+  return (
+    <button className={cl.searchButton}>
+      <span></span>
+    </button>
+  )
+}
+
+export default SearchButton
