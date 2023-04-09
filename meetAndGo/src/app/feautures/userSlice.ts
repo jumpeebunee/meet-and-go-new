ce5c664264650debf444664df42db4cfc4073c2e
Ben <jumpeebunee@gmail.com>
2023-04-09 14:32:12 +0300

    feat: add avatar loading

--- meetAndGo/src/app/feautures/userSlice.ts
+++ meetAndGo/src/app/feautures/userSlice.ts
@@ -17,10 +17,13 @@ const userSlice = createSlice({
     addUser(state, action: PayloadAction<IUser>) {
       state.user = action.payload;
     },
+    updateImage(state, action: PayloadAction<string>) {
+      state.user.image = action.payload;
+    }
   }
 })
 
 export default userSlice.reducer;
-export const {addUser} = userSlice.actions;
+export const { addUser, updateImage } = userSlice.actions;
 export const user = ((state: RootState) => state.user.user);
 export const userImage = ((state: RootState) => state.user.user.image);
--- meetAndGo/src/components/Modals/ProfileModal/ProfileModalUser.tsx
+++ meetAndGo/src/components/Modals/ProfileModal/ProfileModalUser.tsx
@@ -1,5 +1,12 @@
-import { FC } from 'react'
+import { FC, useState } from 'react'
 import cl from './ProfileModal.module.scss'
+import { getDownloadURL, ref, uploadBytesResumable } from 'firebase/storage';
+import { auth, db, storage } from '../../../firebase';
+import { updateProfile } from 'firebase/auth';
+import { doc, updateDoc } from 'firebase/firestore';
+import { useDispatch } from 'react-redux';
+import { updateImage } from '../../../app/feautures/userSlice';
+import { IonSpinner } from '@ionic/react';
 
 interface ProfileModalUserProps {
   image: string;
@@ -8,9 +15,52 @@ interface ProfileModalUserProps {
 }
 
 const ProfileModalUser:FC<ProfileModalUserProps> = ({image, username, raiting}) => {
+
+  const dispatch = useDispatch();
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+    if (!file) return;
+ 
+    const storageRef = ref(storage, username);
+    const uploadTask = uploadBytesResumable(storageRef, file);
+    uploadTask.on('state_changed', 
+      (snapshot) => {},
+      (error) => {}, 
+      () => {
+        setIsLoading(true);
+        getDownloadURL(uploadTask.snapshot.ref).then(async(downloadURL) => {
+          try {
+            const userUid = auth.currentUser?.uid;
+            if (userUid) {
+              dispatch(updateImage(downloadURL));
+              await updateProfile(auth.currentUser as any, { photoURL: downloadURL, })
+              const userRef = doc(db, "users", userUid);
+              await updateDoc(userRef, { image: downloadURL, });
+            }
+          } catch (error) {
+            
+          } finally {
+            setIsLoading(false);
+          }
+        });
+      }
+    );
+  }
+
   return (
     <div className={cl.profileModalUser}>
-      <img className={cl.profileModalImage} src={image} alt="Ваш аватар"/>
+      <div className={isLoading ? `${cl.profileModalImageWrapperLoading} ${cl.profileModalImageWrapper}` : cl.profileModalImageWrapper }>
+        <img className={cl.profileModalImage} src={image} alt="Ваш аватар"/>
+        <span className={cl.profileModalImageUpload}></span>
+        <input disabled={isLoading} accept="image/png, image/jpeg" onChange={(e) => handleFileChange(e)} type="file"/>
+        {isLoading && 
+          <div className={cl.profileModalImageLoading}>
+            <IonSpinner name="crescent"></IonSpinner>
+          </div>
+        }
+      </div>
       <h3>{username}</h3>
       <div className={cl.profileModalRaiting}>
         <div className={cl.profileModalRaitingWrapper}>
