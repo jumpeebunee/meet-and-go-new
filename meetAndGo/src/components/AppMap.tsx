b80812c0d6b219def7eeaf30c4b5a1923229850f
Ben <jumpeebunee@gmail.com>
2023-04-15 12:12:48 +0300

    feat: add events to map

--- meetAndGo/src/components/AppMap.tsx
+++ meetAndGo/src/components/AppMap.tsx
@@ -1,14 +1,13 @@
-import { YMaps, Map } from '@pbe/react-yandex-maps'
-import { API_KEY, MAP_CENTER } from '../data/yamapsApi'
+import { YMaps, Map, Placemark } from '@pbe/react-yandex-maps'
+import { API_KEY, MAP_CENTER, getMapAppMark } from '../data/yamapsApi'
 import cl from '../styles/AppMap.module.scss'
 import EventsButton from './EventsButton'
 import SearchButton from './SearchButton'
 import ProfileButton from './ProfileButton'
-import { FC, useEffect } from 'react'
+import { FC } from 'react'
 import { useSelector } from 'react-redux'
+import { events } from '../app/feautures/eventsSlice'
 import { userImage } from '../app/feautures/userSlice'
-import { collection, onSnapshot } from 'firebase/firestore'
-import { db } from '../firebase'
 
 interface AppMapProps {
   setIsProfileOpen: (arg: boolean) => void;
@@ -19,6 +18,7 @@ interface AppMapProps {
 const AppMap:FC<AppMapProps> = ({setIsProfileOpen, setIsCreateEventOpen, setEventCords}) => {
 
   const image = useSelector(userImage);
+  const currentEvents = useSelector(events);
 
   const createEvent = (e: any) => {
     setIsCreateEventOpen(true);
@@ -27,7 +27,11 @@ const AppMap:FC<AppMapProps> = ({setIsProfileOpen, setIsCreateEventOpen, setEven
   
   return (
     <YMaps query={{apikey: API_KEY}}>
-      <Map onClick={(e: any) => createEvent(e)} className={cl.appMap} defaultState={MAP_CENTER}/>
+      <Map onClick={(e: any) => createEvent(e)} className={cl.appMap} defaultState={MAP_CENTER}>
+        {currentEvents.map(event => 
+          <Placemark key={event.id} options={getMapAppMark(event.placemark)} geometry={event.coords}/>
+        )}
+      </Map>
       <EventsButton/>
       <ProfileButton image={image} setIsProfileOpen={setIsProfileOpen}/>
       <SearchButton/>
--- meetAndGo/src/data/yamapsApi.ts
+++ meetAndGo/src/data/yamapsApi.ts
@@ -6,6 +6,17 @@ export const MAP_APP_LOCATION = {
   iconImageSize: [52, 60],
 }
 
+export const getMapAppMark = (value: number) => {
+
+  const image = `../mark${value}.png`;
+
+  return {
+    iconLayout: 'default#image',
+    iconImageHref: image,
+    iconImageSize: [52, 66],
+  }
+}
+
 export const MAP_CENTER = {
   center: [55.751574, 37.573856],
   zoom: 10,
