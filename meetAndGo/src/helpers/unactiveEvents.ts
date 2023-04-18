7f59b717cb4f5e50c16181ad86b79be8d1d0e50b
Ben <jumpeebunee@gmail.com>
2023-04-18 13:11:37 +0300

    fix: fix removing from array

--- meetAndGo/src/helpers/unactiveEvents.ts
+++ meetAndGo/src/helpers/unactiveEvents.ts
@@ -1,4 +1,4 @@
-import { deleteDoc, doc} from "firebase/firestore";
+import { arrayRemove, deleteDoc, doc, updateDoc} from "firebase/firestore";
 import { IEvent } from "../types/types"
 import { db } from "../firebase";
 
@@ -6,6 +6,15 @@ export const unactiveEvents = async(event: IEvent) => {
   const eventDate = new Date(event.date).getTime();
 
   if (eventDate - Date.now() <= 0) {
-    await deleteDoc(doc(db, "events", event.id));
+    try {
+      for (let user of event.activeUsers) {
+        await updateDoc(doc(db, "users", user.id), {
+          activeMeets: arrayRemove(event.id)
+        });
+      }
+      await deleteDoc(doc(db, "events", event.id));
+    } catch (error) {
+      console.log(error);
+    }
   } 
 }
