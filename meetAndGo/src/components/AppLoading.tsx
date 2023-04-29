96121b48c29f671e6a32f46b809e15148a9b9e98
Ben <jumpeebunee@gmail.com>
2023-04-29 13:32:46 +0300

    feat: change logo

--- meetAndGo/src/components/AppLoading.tsx
+++ meetAndGo/src/components/AppLoading.tsx
@@ -1,9 +1,13 @@
 import cl from '../styles/AppLoading.module.scss'
+import logo from '../assets/logo.svg'
 
 const AppLoading = () => {
   return (
     <div className={cl.appLoading}>
-      <h2>Meet And Go</h2>
+      <div className={cl.appLoadingContent}>
+        <img src={logo} alt="Meet And Go"/>
+        <h2>Meet And Go</h2>
+      </div>
     </div>
   )
 }
